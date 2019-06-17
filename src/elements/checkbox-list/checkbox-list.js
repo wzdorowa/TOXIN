a18b1399a2cec6d3f95d4cd8f2b01e0901659607
w.zdorowa <w.zdorowa@gmail.com>
2019-06-17 14:30:59 +0300

    Добавила поворот значка на 180 градусов при разворачивании списка чекбоксов

--- src/elements/checkbox-list/checkbox-list.js
+++ src/elements/checkbox-list/checkbox-list.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    $('.chechbox-list-title').click(function() {
+    $('.chechbox-list__title').click(function() {
         $('.chechbox-list__slideToggle').slideToggle();
     })
 })
+var checkboxList = document.querySelector(".chechbox-list__title");
+var checkboxRotate = document.querySelector(".chechbox-list__indicator");
+
+checkboxList.onclick = function(){
+    checkboxRotate.classList.toggle('rotate')
+  }
