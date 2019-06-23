ab3dca0a13b784fd6cdb1e96bbbf3cf17b2b724c
w.zdorowa <w.zdorowa@gmail.com>
2019-06-23 13:30:30 +0300

    Подключила скрипт страницы landing к базовому шаблону

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.searchRoom-container_date-group').click(function() {
+    $('.searchRoom-container__date-group').click(function() {
         $('.landing-content__datepicker').slideToggle();
     })
 })
