47a8a5dde2fd4ecea999884a5ae8d445247636cc
w.zdorowa <w.zdorowa@gmail.com>
2020-08-09 17:52:16 +0300

    Внесла изменения на страницу landing

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,5 +1,13 @@
 $(document).ready(() => {
-  $('.search-room__date-group').click(() => {
-    $('.landing__datepicker').slideToggle();
+  const dateInputs = $(document).find('.search-room__input-container');
+  console.log('dateInputs', dateInputs.length);
+  const items = new Array(2);
+  items.forEach(element => {
+    element.click(() => {
+      $('.landing__datepicker').slideToggle();
+    });
   });
+  // $('.search-room__input-container').click(() => {
+  //   $('.landing__datepicker').slideToggle();
+  // });
 });
