01d0fe1d76d8beee2a4a45b6fb6c1221bae114e6
w.zdorowa <w.zdorowa@gmail.com>
2020-08-10 16:21:00 +0300

    исправила ошибку в файле seach-room.js

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -1,7 +1,7 @@
-$(document).ready(() => {
-  document.querySelector('.search-room__filter-access').onclick = e => {
-    const containerUser = document.querySelector('.search-room__filter');
-    containerUser.classList.toggle('search-room__filter_show');
-    e.preventDefault();
-  };
-});
+const element = $('.search-room__filter-access');
+
+element.onclick = event => {
+  const containerUser = $('.search-room__filter');
+  containerUser.classList.toggle('search-room__filter_show');
+  event.preventDefault();
+};
