1cd82eb597f86cea869432c5060fce2d7681ac07
w.zdorowa <w.zdorowa@gmail.com>
2020-08-10 16:03:40 +0300

    удалила лишний неиспользуемый код

--- src/elements/checkbox-list/checkbox-list.js
+++ src/elements/checkbox-list/checkbox-list.js
@@ -2,10 +2,4 @@ $(document).ready(() => {
   $('.checkbox-list__title').click(() => {
     $('.checkbox-list__slide-toggle').slideToggle();
   });
-  const checkboxList = document.querySelector('.checkbox-list__title');
-  const checkboxOpened = document.querySelector('.checkbox-list__indicator');
-
-  checkboxList.onclick = () => {
-    checkboxOpened.classList.toggle('opened');
-  };
 });
