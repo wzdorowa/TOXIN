5f0e8745db1e55e30af9748837d41c30469e419f
w.zdorowa <w.zdorowa@gmail.com>
2020-08-14 13:30:07 +0300

    исправляла именования модификаторов

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,12 @@
 $(document).ready(() => {
   document.querySelector('.header__user-icon').onclick = e => {
     const containerUser = document.querySelector('.header__user');
-    containerUser.classList.toggle('header__user-icon_show');
+    containerUser.classList.toggle('header__user-icon_visible');
     e.preventDefault();
   };
   document.querySelector('.header__navigation-icon').onclick = e => {
     const containerUser = document.querySelector('.header__navigation');
-    containerUser.classList.toggle('header__navigation-icon_show');
+    containerUser.classList.toggle('header__navigation-icon_visible');
     e.preventDefault();
   };
 });
