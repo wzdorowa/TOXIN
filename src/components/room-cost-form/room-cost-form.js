4defad85df7fdf33d8b6a52a03f37557cdee2d42
w.zdorowa <w.zdorowa@gmail.com>
2020-12-05 20:10:36 +0300

    Доработала закрывание календаря #15

--- src/components/room-cost-form/room-cost-form.js
+++ src/components/room-cost-form/room-cost-form.js
@@ -3,11 +3,15 @@ const elements = document.querySelectorAll('.room-cost-form__input-container');
 elements.forEach(element => {
   const elementInput = element.querySelector('.input__content');
   const datepicker = document.querySelector('.room-cost-form__datepicker');
+  const buttonApply = datepicker.querySelector(
+    '.calendar__buttons-container_for-apply',
+  );
 
-  elementInput.onfocus = () => {
+  elementInput.addEventListener('focus', () => {
     datepicker.classList.add('room-cost-form__datepicker_visible');
-  };
-  elementInput.onblur = () => {
+  });
+  buttonApply.addEventListener('click', e => {
+    e.preventDefault();
     datepicker.classList.remove('room-cost-form__datepicker_visible');
-  };
+  });
 });
