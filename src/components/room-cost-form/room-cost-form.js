17208aba8517a31174d21c7bdd9279cb6abdb61d
w.zdorowa <w.zdorowa@gmail.com>
2020-12-05 16:26:25 +0300

    исправила ошибку в работе checkbox-list #15

--- src/components/room-cost-form/room-cost-form.js
+++ src/components/room-cost-form/room-cost-form.js
@@ -1,11 +1,8 @@
 const elements = document.querySelectorAll('.room-cost-form__input-container');
-console.log('elements', elements);
 
 elements.forEach(element => {
   const elementInput = element.querySelector('.input__content');
-  console.log('elementInput', elementInput);
   const datepicker = document.querySelector('.room-cost-form__datepicker');
-  console.log('datepicker', datepicker);
 
   elementInput.onfocus = () => {
     datepicker.classList.add('room-cost-form__datepicker_visible');
--- src/elements/checkbox-list/checkbox-list.js
+++ src/elements/checkbox-list/checkbox-list.js
@@ -1,5 +1,15 @@
-$(document).ready(() => {
-  $('.checkbox-list__title').click(() => {
-    $('.checkbox-list__slide-toggle').slideToggle();
-  });
+// $(document).ready(() => {
+//   $('.checkbox-list__title').click(() => {
+//     $('.checkbox-list__slide-toggle').slideToggle();
+//   });
+// });
+const checkboxLists = document.querySelectorAll('.checkbox-list');
+
+checkboxLists.forEach(element => {
+  const checkboxListTitle = element.querySelector('.checkbox-list__title');
+  const checkboxListToggle = element.querySelector('.checkbox-list__toggle');
+
+  checkboxListTitle.onclick = () => {
+    checkboxListToggle.classList.toggle('checkbox-list__toggle');
+  };
 });
