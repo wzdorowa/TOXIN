0cb93a8ed71a8a01841c542ac0e084d69cfb44f0
w.zdorowa <w.zdorowa@gmail.com>
2020-12-05 20:00:26 +0300

    доработала закрывание календаря #15

--- src/components/form-check-in/form-check-in.js
+++ src/components/form-check-in/form-check-in.js
@@ -1,13 +1,17 @@
-const elements = document.querySelectorAll('.check-in__input-container');
+const inputs = document.querySelectorAll('.check-in__input-container');
 
-elements.forEach(element => {
+inputs.forEach(element => {
   const elementInput = element.querySelector('.input__content');
   const datepicker = document.querySelector('.check-in__datepicker');
+  const buttonApply = datepicker.querySelector(
+    '.calendar__buttons-container_for-apply',
+  );
 
-  elementInput.onfocus = () => {
+  elementInput.addEventListener('focus', () => {
     datepicker.classList.add('check-in__datepicker_visible');
-  };
-  elementInput.onblur = () => {
+  });
+  buttonApply.addEventListener('click', e => {
+    e.preventDefault();
     datepicker.classList.remove('check-in__datepicker_visible');
-  };
+  });
 });
