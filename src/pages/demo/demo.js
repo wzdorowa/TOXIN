0bb5ff8cf804ca770c97d8da11b96091c5dd09e6
w.zdorowa <w.zdorowa@gmail.com>
2020-12-05 20:49:55 +0300

    Доработала закрывание календаря #15

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -7,18 +7,22 @@ elements.forEach((element, index) => {
   const elementsInput = element.querySelectorAll('.input__content');
 
   elementsInput.forEach(elementInput => {
-    elementInput.onfocus = () => {
+    elementInput.addEventListener('focus', () => {
       datepicker[index].classList.add(
         'inputs-with-datepicker__datepicker_visible',
       );
-    };
+    });
   });
 
-  elementsInput.forEach(elementInput => {
-    elementInput.onblur = () => {
+  datepicker.forEach(element => {
+    const buttonApply = element.querySelector(
+      '.calendar__buttons-container_for-apply',
+    );
+    buttonApply.addEventListener('click', e => {
+      e.preventDefault();
       datepicker[index].classList.remove(
         'inputs-with-datepicker__datepicker_visible',
       );
-    };
+    });
   });
 });
