958641e6040a34d3020a5031c208a537c92a47a9
w.zdorowa <w.zdorowa@gmail.com>
2020-12-08 15:24:19 +0300

    Добавила возможность открытия дропдауна при клике на стрелку #15

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -2,12 +2,24 @@ const elements = document.querySelectorAll('.dropdown');
 
 elements.forEach(element => {
   const elementInput = element.querySelector('.input__content');
-  const dropDownList = element.querySelector('.dropdown__content');
+  const dropdownList = element.querySelector('.dropdown__content');
+  const dropdownArrow = element.querySelector('.dropdown__list-arrow-down');
 
+  dropdownArrow.addEventListener('click', () => {
+    if (
+      dropdownList.classList.contains('dropdown__content_visible') === false
+    ) {
+      dropdownList.classList.add('dropdown__content_visible');
+    } else if (dropdownList.classList.contains('dropdown__content_visible')) {
+      dropdownList.classList.remove('dropdown__content_visible');
+    }
+  });
   elementInput.onfocus = () => {
-    dropDownList.classList.add('dropdown__content_visible');
+    dropdownList.classList.add('dropdown__content_visible');
   };
-  elementInput.onblur = () => {
-    dropDownList.classList.remove('dropdown__content_visible');
-  };
+  document.addEventListener('click', event => {
+    if (event.target.closest('.dropdown') !== element) {
+      dropdownList.classList.remove('dropdown__content_visible');
+    }
+  });
 });
