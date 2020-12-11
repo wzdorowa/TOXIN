0ec7562833f314826f22e11406891a5258cfeb4a
w.zdorowa <w.zdorowa@gmail.com>
2020-12-11 13:10:44 +0300

    установила и подключила плагин Masked Input для полей с маской ввода #15

--- src/components/form-check-in/form-check-in.js
+++ src/components/form-check-in/form-check-in.js
@@ -1,1 +1,6 @@
-
+const maskedContainer = document.querySelectorAll(
+  '.check-in__input-container_for-masked-field',
+);
+maskedContainer.forEach(() => {
+  $('.input__content').mask('99.99.9999', { placeholder: 'ДД.ММ.ГГГГ' });
+});
--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -49,3 +49,8 @@ elements.forEach((element, index) => {
     });
   });
 });
+
+const maskedContainer = document.querySelectorAll('.inputs__item-masked-field');
+maskedContainer.forEach(() => {
+  $('.input__content').mask("99.99.9999", {placeholder: "ДД.ММ.ГГГГ"});
+});
