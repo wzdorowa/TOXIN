dab18be6065d47708f61e977055fedccc89415cc
w.zdorowa <w.zdorowa@gmail.com>
2020-12-16 15:16:00 +0300

    дописала скрипт для работы дропдауна

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,11 +1,14 @@
 const elements = document.querySelectorAll('.dropdown');
 
-elements.forEach((element, index) => {
+elements.forEach(element => {
   const elementInput = element.querySelector('.input__content');
   const dropdownList = element.querySelector('.dropdown__content');
   const dropdownArrow = element.querySelector('.input__icon-arrow-down');
   const buttonApply = element.querySelector(
     '.dropdown__buttons-container_for-apply',
+  );
+  const buttonClear = element.querySelector(
+    '.dropdown__buttons-container_for-clear',
   );
 
   dropdownArrow.addEventListener('click', () => {
@@ -25,63 +28,113 @@ elements.forEach((element, index) => {
       dropdownList.classList.remove('dropdown__content_visible');
     }
   });
-  const signsGroup = element.querySelectorAll('.dropdown-row__amount-counter');
-  console.log('signsGroup[index]', signsGroup[index]);
-  signsGroup[index].addEventListener('click', e => {
-    e.preventDefault();
+  const rowsGroupParent = element.querySelector('.dropdown__rows');
+  const rowsGroup = element.querySelectorAll('.dropdown-row');
+  const numbers = element.querySelectorAll('.dropdown-row__amount_with-count');
 
-    const numbers = element.querySelectorAll(
-      '.dropdown-row__amount_with-count',
-    );
-
-    const countValues = value => {
-      if (value === 0) {
-        elementInput.value += 'Сколько гостей';
-      } else if (String(value).includes('1')) {
-        elementInput.value += `${String(value)} гость`;
-      } else if (
-        String(value).includes('2') ||
-        String(value).includes('3') ||
-        String(value).includes('4')
-      ) {
-        elementInput.value += `${String(value)} гостя`;
-      } else {
-        elementInput.value += `${String(value)} гостей`;
+  const countValues = (result, values, declinations) => {
+    if (result === 0) {
+      elementInput.value = '';
+    } else {
+      const newString = [];
+      values.forEach((value, i) => {
+        const declination = declinations[i];
+        if (String(value).includes('1')) {
+          newString[i] = `${String(value)} ${declination[0]}`;
+        } else if (
+          String(value).includes('2') ||
+          String(value).includes('3') ||
+          String(value).includes('4')
+        ) {
+          newString[i] = `${String(value)} ${declination[1]}`;
+        } else {
+          newString[i] = `${String(value)} ${declination[2]}`;
+        }
+      });
+      newString.forEach((string, index) => {
+        if (index < newString.length - 1) {
+          elementInput.value += `${newString[index]}, `;
+        } else if (index === newString.length - 1) {
+          elementInput.value += `${newString[index]}`;
+        }
+      });
+    }
+  };
+  const countTheGuests = () => {
+    let adults = 0;
+    let babies = 0;
+    numbers.forEach((number, index) => {
+      if (index <= 1) {
+        adults += Number(number.innerHTML);
+      } else if (index === 2) {
+        babies += Number(number.innerHTML);
       }
-    };
-    const countTheGuests = () => {
-      let adults = 0;
-      let babies = 0;
-      numbers.forEach((number, index) => {
-        if (index <= 1) {
-          adults += Number(number.innerHTML);
-        } else if (index === 2) {
-          babies += Number(number.innerHTML);
-        }
-        countValues(adults);
-        countValues(babies);
-      });
-    };
-    const countAmenities = () => {
-      let bedrooms = 0;
-      let beds = 0;
-      let bathrooms = 0;
+    });
+    const amountGuests = adults + babies;
+    const guests = [adults, babies];
+    const declinationGuests = ['гость', 'гостя', 'гостей'];
+    const declinationBabies = ['младенец', 'младенца', 'младенцев'];
+    const declinations = [declinationGuests, declinationBabies];
+    countValues(amountGuests, guests, declinations);
+  };
+  const countAmenities = () => {
+    let bedrooms = 0;
+    let beds = 0;
+    let bathrooms = 0;
 
-      numbers.forEach((number, index) => {
-        if (index <= 1) {
-          bedrooms += Number(number.innerHTML);
-        } else if (index === 2) {
-          beds += Number(number.innerHTML);
-        } else if (index === 2) {
-          bathrooms += Number(number.innerHTML);
-        }
-        countValues(bedrooms);
-        countValues(beds);
-        countValues(bathrooms);
-      });
-    };
+    numbers.forEach((number, index) => {
+      if (index === 0) {
+        bedrooms += Number(number.innerHTML);
+      } else if (index === 1) {
+        beds += Number(number.innerHTML);
+      } else if (index === 2) {
+        bathrooms += Number(number.innerHTML);
+      }
+    });
+    const amountAmenities = bedrooms + beds + bathrooms;
+    const amenities = [bedrooms, beds, bathrooms];
+    const declinationBedrooms = ['спальня', 'спальни', 'спален'];
+    const declinationBeds = ['кровать', 'кровати', 'кроватей'];
+    const declinationBathrooms = [
+      'ванная комната',
+      'ванные комнаты',
+      'ванных комнат',
+    ];
+    const declinations = [
+      declinationBedrooms,
+      declinationBeds,
+      declinationBathrooms,
+    ];
+    countValues(amountAmenities, amenities, declinations);
+  };
+  rowsGroup.forEach(element => {
+    element.addEventListener('click', e => {
+      e.preventDefault();
+
+      elementInput.value = null;
+      if (
+        rowsGroupParent.classList.contains(
+          'dropdown__rows_for-count-the-guests',
+        ) === true
+      ) {
+        countTheGuests();
+      } else if (
+        rowsGroupParent.classList.contains(
+          'dropdown__rows_for-count-amenities',
+        ) === true
+      ) {
+        countAmenities();
+      }
+    });
   });
   buttonApply.addEventListener('click', () => {
     dropdownList.classList.remove('dropdown__content_visible');
+  });
+  buttonClear.addEventListener('click', () => {
+    elementInput.value = '';
+    numbers.forEach(number => {
+      const element = number;
+      element.innerHTML = '0';
+    });
   });
 });
