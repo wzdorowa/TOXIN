5dbb2edcb69853ba58c7ae16aa53d8378d65f91f
w.zdorowa <w.zdorowa@gmail.com>
2020-12-29 11:50:12 +0300

    реализовала возможность закрываться expandable checkbox list при клике вне него #15

--- src/elements/checkbox-list/checkbox-list.js
+++ src/elements/checkbox-list/checkbox-list.js
@@ -2,9 +2,17 @@ const checkboxLists = document.querySelectorAll('.checkbox-list');
 
 checkboxLists.forEach(element => {
   const checkboxListTitle = element.querySelector('.checkbox-list__title');
-  const checkboxListToggle = element.querySelector('.checkbox-list__toggle');
+  const checkboxListToggle = element.querySelector(
+    '.checkbox-list__container_hidden',
+  );
 
   checkboxListTitle.onclick = () => {
-    checkboxListToggle.classList.toggle('checkbox-list__toggle');
+    checkboxListToggle.classList.toggle('checkbox-list__container_hidden');
   };
+
+  document.addEventListener('click', event => {
+    if (event.target.closest('.checkbox-list') !== element) {
+      checkboxListToggle.classList.add('checkbox-list__container_hidden');
+    }
+  });
 });
