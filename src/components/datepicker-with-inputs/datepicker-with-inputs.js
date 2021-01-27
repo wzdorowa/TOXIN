c0ef44b467cb21310644b5de0a802e87037f6611
w.zdorowa <w.zdorowa@gmail.com>
2021-01-27 15:04:50 +0300

    переписала js-код компонента datepicker-with-nputs с использованием es6 классов. #18

--- src/components/datepicker-with-inputs/datepicker-with-inputs.js
+++ src/components/datepicker-with-inputs/datepicker-with-inputs.js
@@ -1,130 +1,162 @@
-const elements = document.querySelectorAll('.datepicker-group');
-const datepicker = document.querySelectorAll('.datepicker-group__datepicker');
+class Datepicker {
+  constructor(element) {
+    this.datepickerGroup = element;
+    this.datepicker = null;
+    this.elementsInput = null;
+    this.dropdownsArrow = null;
+    this.buttonApply = null;
+    this.buttonClear = null;
+    this.calendar = null;
 
-elements.forEach((element, index) => {
-  const elementsInput = element.querySelectorAll('.input__content');
-  const dropdownsArrow = element.querySelectorAll('.input__icon-arrow-down');
+    this.findElement();
+    this.listenFocusInput();
+    this.listenClickDropdownArrow();
+    this.listenButtonApply();
+    this.listenButtonClear();
+    this.listenClickDocument();
+    this.setValueToInput();
+  }
 
-  document.addEventListener(
-    'click',
-    event => {
-      const hide = () => {
-        if (
-          datepicker[index].classList.contains(
-            'datepicker-group__datepicker_visible',
-          )
-        ) {
-          datepicker[index].classList.remove(
-            'datepicker-group__datepicker_visible',
-          );
-        }
-      };
-      if (!element.contains(event.target)) {
-        hide();
-      }
-    },
-    true,
-  );
-  dropdownsArrow.forEach(dropdownArrow => {
-    const addOrRemoveClass = () => {
-      if (
-        datepicker[index].classList.contains(
-          'datepicker-group__datepicker_visible',
-        ) === false
-      ) {
-        datepicker[index].classList.add('datepicker-group__datepicker_visible');
-      } else if (
-        datepicker[index].classList.contains(
-          'datepicker-group__datepicker_visible',
-        )
-      ) {
-        datepicker[index].classList.remove(
-          'datepicker-group__datepicker_visible',
-        );
-      }
-    };
-    dropdownArrow.addEventListener('click', () => {
-      addOrRemoveClass();
-    });
-  });
-
-  elementsInput.forEach(elementInput => {
-    const addClass = () => {
-      datepicker[index].classList.add('datepicker-group__datepicker_visible');
-    };
-    elementInput.addEventListener('focus', () => {
-      addClass();
-    });
-  });
-
-  datepicker.forEach((element, index) => {
-    const buttonApply = element.querySelector(
+  findElement() {
+    this.datepicker = this.datepickerGroup.querySelector(
+      '.datepicker-group__datepicker',
+    );
+    this.elementsInput = this.datepickerGroup.querySelectorAll(
+      '.input__content',
+    );
+    this.dropdownsArrow = this.datepickerGroup.querySelectorAll(
+      '.input__icon-arrow-down',
+    );
+    this.buttonApply = this.datepickerGroup.querySelector(
       '.calendar__buttons-container_for-apply',
     );
-    const buttonClear = element.querySelector(
+    this.buttonClear = this.datepickerGroup.querySelector(
       '.calendar__buttons-container_for-clear',
     );
-    const applyDatepicker = e => {
-      e.preventDefault();
-      datepicker[index].classList.remove(
+    this.calendar = this.datepickerGroup.querySelector('.calendar__content');
+  }
+
+  addClass() {
+    this.datepicker.classList.add('datepicker-group__datepicker_visible');
+  }
+
+  removeClass() {
+    this.datepicker.classList.remove('datepicker-group__datepicker_visible');
+  }
+
+  toggleClass() {
+    if (
+      this.datepicker.classList.contains(
         'datepicker-group__datepicker_visible',
-      );
-    };
-    const clearDatepicker = e => {
-      e.preventDefault();
-      const myCalendar = $(element).find('.calendar__content');
-      const calendar = $(myCalendar).datepicker().data('datepicker');
-      calendar.clear();
-    };
-    buttonApply.addEventListener('click', e => {
-      applyDatepicker(e);
-    });
-    buttonClear.addEventListener('click', e => {
-      clearDatepicker(e);
-    });
-  });
-  const calendar = element.querySelector('.calendar__content');
-  if ($(calendar).parents('.datepicker-group__datepicker_only').length === 1) {
-    $(calendar).datepicker({
-      navTitles: {
-        days: 'MM <i>yyyy</i>',
-      },
-      multipleDatesSeparator: ' - ',
-      dateFormat: 'dd M',
-      onSelect: (fd, dates) => {
-        const res = element.querySelector('.input__content');
-        let string = '';
+      ) === false
+    ) {
+      this.addClass();
+    } else if (
+      this.datepicker.classList.contains('datepicker-group__datepicker_visible')
+    ) {
+      this.removeClass();
+    }
+  }
 
-        if (dates) {
-          dates.forEach(() => {
-            string = fd;
-          });
-          res.value = string;
-        }
-      },
-    });
-  } else {
-    $(calendar).datepicker({
-      navTitles: {
-        days: 'MM <i>yyyy</i>',
-      },
-      dateFormat: 'dd.mm.yyyy',
-      onSelect: fd => {
-        const res = element.querySelectorAll('.input__content');
-        let dayFrom = '';
-        let dayTo = '';
+  clearValues() {
+    const $myCalendar = $(this.datepickerGroup).find('.calendar__content');
+    const $calendar = $($myCalendar).datepicker().data('datepicker');
+    $calendar.clear();
+  }
 
-        const signs = fd.split('');
-        signs.forEach((element, i) => {
-          if (i < 10) {
-            dayFrom += element;
-          } else if (i > 10) {
-            dayTo += element;
-          }
-        });
-        res[0].value = dayFrom;
-        res[1].value = dayTo;
-      },
+  listenFocusInput() {
+    this.elementsInput.forEach(elementInput => {
+      elementInput.addEventListener('focus', () => {
+        this.addClass();
+      });
     });
   }
+
+  listenClickDropdownArrow() {
+    this.dropdownsArrow.forEach(dropdownArrow => {
+      dropdownArrow.addEventListener('click', () => {
+        this.toggleClass();
+      });
+    });
+  }
+
+  listenButtonApply() {
+    this.buttonApply.addEventListener('click', () => {
+      this.removeClass();
+    });
+  }
+
+  listenButtonClear() {
+    this.buttonClear.addEventListener('click', () => {
+      this.clearValues();
+    });
+  }
+
+  listenClickDocument() {
+    document.addEventListener(
+      'click',
+      event => {
+        if (!this.datepickerGroup.contains(event.target)) {
+          this.removeClass();
+        }
+      },
+      true,
+    );
+  }
+
+  setValueToInput() {
+    if (
+      $(this.calendar).parents('.datepicker-group__datepicker_only').length ===
+      1
+    ) {
+      $(this.calendar).datepicker({
+        navTitles: {
+          days: 'MM <i>yyyy</i>',
+        },
+        multipleDatesSeparator: ' - ',
+        dateFormat: 'dd M',
+        onSelect: (fd, dates) => {
+          const input = this.datepickerGroup.querySelector('.input__content');
+          let string = '';
+
+          if (dates) {
+            dates.forEach(() => {
+              string = fd;
+            });
+            input.value = string;
+          }
+        },
+      });
+    } else {
+      $(this.calendar).datepicker({
+        navTitles: {
+          days: 'MM <i>yyyy</i>',
+        },
+        dateFormat: 'dd.mm.yyyy',
+        onSelect: fd => {
+          const inputs = this.datepickerGroup.querySelectorAll(
+            '.input__content',
+          );
+          let dayFrom = '';
+          let dayTo = '';
+
+          const signs = fd.split('');
+          signs.forEach((element, i) => {
+            if (i < 10) {
+              dayFrom += element;
+            } else if (i > 10) {
+              dayTo += element;
+            }
+          });
+          inputs[0].value = dayFrom;
+          inputs[1].value = dayTo;
+        },
+      });
+    }
+  }
+}
+
+const elements = document.querySelectorAll('.datepicker-group');
+elements.forEach(element => {
+  new Datepicker(element);
 });
