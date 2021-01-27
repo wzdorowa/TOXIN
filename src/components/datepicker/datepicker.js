34e676047c687a62ebe89e69d1e36f54d492eaa6
w.zdorowa <w.zdorowa@gmail.com>
2021-01-27 13:15:39 +0300

    удалила файл с бесполезным кодом

--- src/components/datepicker/datepicker.js
+++
@@ -1,9 +0,0 @@
-const calendar = document.querySelectorAll('.datepicker-here');
-calendar.forEach(element => {
-  $(element).datepicker({
-    navTitles: {
-      days: 'MM <i>yyyy</i>',
-    },
-    dateFormat: 'dd.mm.yyyy',
-  });
-});
