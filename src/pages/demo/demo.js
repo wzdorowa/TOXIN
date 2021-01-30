566f307106061a2a4684ec62e3f61c370aa02794
w.zdorowa <w.zdorowa@gmail.com>
2021-01-30 15:52:42 +0300

    применила класс MaskedInput к инпуту на странице demo

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -1,8 +1,8 @@
+import MaskedInput from '../../js/masked-input';
+
 const maskedContainer = document.querySelectorAll(
   '.components__inputs-item-masked-field',
 );
-
 maskedContainer.forEach(element => {
-  const maskedForDate = element.querySelector('.input__content');
-  $(maskedForDate).inputmask('99.99.9999', { placeholder: 'ДД/ММ/ГГГГ' });
+  new MaskedInput(element);
 });
