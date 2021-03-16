91c3c0b9538dff258892e050e51fd523f1ab9042
w.zdorowa <w.zdorowa@gmail.com>
2021-03-16 21:07:52 +0300

    переименовала все миксины у которых не совпадало название возвращаемого блока. #17

--- src/components/form-check-in/form-check-in.js
+++ src/components/form-check-in/form-check-in.js
@@ -1,7 +1,7 @@
 import MaskedInput from '../../js/masked-input';
 
 const maskedContainer = document.querySelectorAll(
-  '.js-check-in__input-container_for-masked-field',
+  '.js-form-check-in__input-container_for-masked-field',
 );
 maskedContainer.forEach(element => {
   new MaskedInput(element);
