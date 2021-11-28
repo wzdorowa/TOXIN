58f271aaf9795616b57547cabaffefd3eb0e8ea0
w.zdorowa <w.zdorowa@gmail.com>
2021-11-28 15:32:42 +0300

    Доработала футер

--- src/components/footer/footer.js
+++
@@ -1,6 +0,0 @@
-import InputMask from '../../elements/InputMask/InputMask';
-
-const elements = document.querySelectorAll('.js-footer__subscribe-input');
-elements.forEach(element => {
-  new InputMask(element);
-});
