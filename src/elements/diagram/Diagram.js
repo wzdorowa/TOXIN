43f06e058ca86ce4d36167622f41361ff64a8b7c
w.zdorowa <w.zdorowa@gmail.com>
2021-11-28 14:42:19 +0300

    Доработала диаграмму

--- src/elements/diagram/Diagram.js
+++ src/elements/diagram/Diagram.js
@@ -2,21 +2,21 @@ class Diagram {
   constructor(element) {
     this.item = element;
 
-    this.findElement();
-    this.bindEventListeners();
+    this._findElement();
+    this._bindEventListeners();
   }
 
-  findElement() {
-    this.segments = document.querySelectorAll('.room-details__donut-segment');
+  _findElement() {
+    this.segments = document.querySelectorAll('.diagram__image-circle');
     this.votesValue = document.querySelector('.diagram__number');
   }
 
-  bindEventListeners() {
-    const { color } = this.item.dataset;
+  _bindEventListeners() {
+    const { impression } = this.item.dataset;
     let segment;
     this.segments.forEach(element => {
-      const colorSegment = element.dataset.color;
-      if (colorSegment === color) {
+      const impressionSegment = element.dataset.impression;
+      if (impressionSegment === impression) {
         segment = element;
       }
       return null;
@@ -25,12 +25,12 @@ class Diagram {
     if (segment !== undefined) {
       this.item.addEventListener('mouseover', () => {
         const { votes } = segment.dataset;
-        segment.setAttribute('stroke-width', '2');
+        segment.setAttribute('stroke-width', '7');
         this.votesValue.innerHTML = votes;
       });
       this.item.addEventListener('mouseout', () => {
         const { total } = segment.dataset;
-        segment.setAttribute('stroke-width', '1');
+        segment.setAttribute('stroke-width', '4');
         this.votesValue.innerHTML = total;
       });
     }
@@ -39,7 +39,7 @@ class Diagram {
 
 const diagram = document.querySelector('.diagram');
 if (diagram !== null) {
-  const items = diagram.querySelectorAll('.bulleted-list__item');
+  const items = diagram.querySelectorAll('.diagram__item');
   items.forEach(item => {
     new Diagram(item);
   });
