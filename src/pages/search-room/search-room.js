d2113eadbcba8cb7bbb1ffb94ba2cb372d29de69
w.zdorowa <w.zdorowa@gmail.com>
2021-12-15 17:37:17 +0300

    Доработала отображение блока фильтров на маленьких разрешениях. Подогнала верстку под pixel perfect.

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -1,5 +1,7 @@
-$(document).ready(() => {
-  $('.js-search-room__filter-access').click(() => {
-    $('.js-search-room__filter').slideToggle();
-  });
+const filterButton = document.querySelector('.js-search-room__filter-button');
+const filter = document.querySelector('.js-search-room__filter');
+
+filterButton.addEventListener('click', () => {
+  filter.classList.toggle('search-room__filter_open');
+  filterButton.classList.toggle('search-room__filter-button_open');
 });
