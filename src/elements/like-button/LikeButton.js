5dbb37d4942259097bd6ae0024bd3f4253a4c5eb
w.zdorowa <w.zdorowa@gmail.com>
2021-12-24 15:26:42 +0300

    Доработала доступность компонента like-button с клавиатуры

--- src/elements/like-button/LikeButton.js
+++ src/elements/like-button/LikeButton.js
@@ -1,3 +1,5 @@
+const { event } = require("jquery");
+
 class LikeButton {
   constructor(element) {
     this.buttonContainer = element;
@@ -17,17 +19,27 @@ class LikeButton {
     );
   }
 
+  _handleKeyDown() {
+    if (this.likeButton.checked === true) {
+      this.description.innerHTML = Number(this.description.innerHTML) - 1;
+      this.likeButton.checked = false;
+    } else if (this.likeButton.checked === false) {
+      this.description.innerHTML = Number(this.description.innerHTML) + 1;
+      this.likeButton.checked = true;
+    }
+  }
+
   _handleClick() {
     if (this.likeButton.checked === true) {
       this.description.innerHTML = Number(this.description.innerHTML) + 1;
-    }
-    if (this.likeButton.checked === false) {
+    } else if (this.likeButton.checked === false) {
       this.description.innerHTML = Number(this.description.innerHTML) - 1;
     }
   }
 
   _bindEventListener() {
     this.likeButton.addEventListener('click', this._handleClick.bind(this));
+    this.likeButton.addEventListener('keydown', this._handleKeyDown.bind(this));
   }
 }
 
