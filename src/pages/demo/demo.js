1b1a6a7ca6041a21504f1868502a28771bd85c3a
w.zdorowa <w.zdorowa@gmail.com>
2022-02-01 12:25:33 +0300

    Удалила файл demo.js.

--- src/pages/demo/demo.js
+++
@@ -1,9 +0,0 @@
-const elements = document.querySelectorAll('.js-datepicker-here');
-
-elements.forEach(element => {
-  $(element).datepicker({
-    navTitles: {
-      days: 'MM <i>yyyy</i>',
-    }
-  });
-});
